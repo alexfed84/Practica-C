2d1f4bf0c16015057c8f3b440bb938fc4681ac83
alexfed84 <alfed09@gmail.com>
2022-02-08 12:27:49 +0300

    added task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача № 3: Даны два числа. Показать большее и меньшее число
+
+int a = 0;
+int b = 0;
+string c;
+
+Console.WriteLine("Введите число a");
+ c = Console.ReadLine();
+ a = Convert.ToInt32(c);
+
+Console.WriteLine("Введите число b");
+ c = Console.ReadLine();
+ b = Convert.ToInt32(c);
+
+if (a > b)
+{
+    Console.WriteLine("Число a больше числа b");
+}
+else if (a < b)
+{
+    Console.WriteLine ("Число a меньше числа b");
+}    
+else 
+{
+    Console.WriteLine ("Число a равно числу b");
+}
+
