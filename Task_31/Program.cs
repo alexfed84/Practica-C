1d37855855b8079f4e071dc2fef7b4cb0a3ce5aa
alexfed84 <alfed09@gmail.com>
2022-02-08 12:47:08 +0300

    added task_31

---
+++ Task_31/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача № 31: Задать массив из 8 элементов и вывести их на экран.
+
+int [] a = new int[8]{1,2,3,4,5,6,7,98}; // заполнение массива в ручную.
+for (int i = 0; i<a.Length; i++)
+Console.Write($"{a[i]} ");
+
+
+int [] a = new int[8]; // рандомный случай заполнения массива.
+for (int i = 0; i<a.Length; i++)
+ a[i] = new Random().Next (0, 20);
+for (int i = 0; i<a.Length; i++)
+Console.Write($"{a[i]} ");
+
