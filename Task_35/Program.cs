0fd3df77ca626ebc704c25d42fa07598521ddfee
alexfed84 <alfed09@gmail.com>
2022-02-08 12:57:13 +0300

    added task_35

---
+++ Task_35/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача №35:Определить, присутствует ли в заданном массиве, некоторое число.
+
+int [] a = {1,4,6,13,8,23,34};
+int find = 8;
+int n = a.Length;
+int index = 0;
+while (index < n)
+{
+    if (a[index]==find)
+    {
+        Console.WriteLine(index);
+    }
+    index++; //index = index + 1;
+    
+}
+
