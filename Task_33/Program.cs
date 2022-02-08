51c06abd3b0eb247db9db1a4259ea29b78108b34
alexfed84 <alfed09@gmail.com>
2022-02-08 12:51:56 +0300

    added task_33

---
+++ Task_33/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №33:Задать массив из 12 элементов, заполненных числами из [-9, 9]. Найти сумму положительных/отрицательных элементов массива
+
+System.Int32[] a = {-8,-7,-5,-4,-3,-1,3,4,5,6,8,9};
+int sumPos = 0;
+int sumNeg = 0;
+for (int i=0; i < a.Length; i++)
+{
+   if (a[i]>0) sumPos =sumPos + a[i];
+   if (a[i]<0) sumNeg = sumNeg + a[i];
+}
+Console.WriteLine(sumPos);
+Console.WriteLine(sumNeg);
