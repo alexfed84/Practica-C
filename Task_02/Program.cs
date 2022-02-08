2c3b97de453485def7a8ab25481c0bdf26c64d9a
alexfed84 <alfed09@gmail.com>
2022-02-08 12:25:21 +0300

    added task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача № 2: По двум заданным числам проверять является ли первое квадратом второго.
+
+// Метод 1
+
+double a = 5;
+double b = 16;
+if (a*a == b)  Console.WriteLine("a  является квадрантом b");
+if (a*a != b) Console.WriteLine("a не является квадрантом b");
+
+// Метод 2
+
+double a = 0;
+double b = 0;
+string c;
+
+Console.WriteLine("Введите число a:");
+c = Console.ReadLine();
+a = Convert.ToDouble(c);
+
+Console.WriteLine("Введите  число b:");
+c = Console.ReadLine();
+b = Convert.ToDouble(c);
+
+if (a*a ==b)                  //(a == Math.Pow(b,2)) ??
+{
+Console.WriteLine (" a квадрант b ");
+}
+else 
+{
+ Console.WriteLine (" a не квадрант b ");
+}
