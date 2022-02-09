953d248c4bf23d2e5fab0d879f6509f88e7897a4
alexfed84 <alfed09@gmail.com>
2022-02-09 11:09:21 +0300

    added task_09

---
+++ Task_09/Program.cs
@@ -0,0 +1,20 @@
+﻿int a=0; 
+int b=0;
+string c;
+
+Console.WriteLine("Введите число в последовательности от 1:");
+c=Console.ReadLine();
+a=Convert.ToInt32(c);
+
+Console.WriteLine("Введите число в последовательности до N:");
+c=Console.ReadLine();
+b=Convert.ToInt32(c);
+
+while (a!=b)
+{
+    if (a%2 == 0)
+    Console.Write($"{a}" + " ");
+
+    a++;
+
+}
