02dbf8eb0dd75a2eb6a1a6e44dc6dc60fd5f1192
alexfed84 <alfed09@gmail.com>
2022-02-09 13:33:50 +0300

    added task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача № 27:	Определить количество цифр в числе
+
+string text ="456547"; 
+
+int lot (string text)
+{
+    int count  = 0;
+    for (int i = 0; i < text.Length; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Количество цифр в данном числе:"  + lot(text));
