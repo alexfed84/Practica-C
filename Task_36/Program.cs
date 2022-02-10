197e88d661313cecdf56e04781d887d284855d40
alexfed84 <alfed09@gmail.com>
2022-02-10 17:14:22 +0300

    added task_36

--- Task_36/Program.cs
+++ Task_36/Program.cs
@@ -1,31 +1,16 @@
-﻿//Задача №36:	Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
-/*
-int [] array = new int[10];
+﻿ //Задача №36:	Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
 
-void FillArray(int [] collection)
+int [] a = new int[7];
+int chet = 0;
+int nechet = 0;
+
+for (int i=0; i < a.Length; i++)
 {
-   int lenght = collection.Length;  // длинна массива
-   int index = 0;
-   while (index < lenght)
-   {
-       collection [index] = new Random().Next(100,110);
-       index++; //index = index + 1
-   } 
+    a[i] = new Random().Next(100,200);
+    if (a[i]%2 ==0) chet ++;
+    if (a[i]%2 !=0) nechet ++;
+    
 }
 
-void PrintArray(int[] col)
-{ 
-     int count = col.Length;
-     int position = 0;
-     while (position < count)
-     {
-         Console.WriteLine(col[position]);
-         position++;
-         if (array[position]%2==0) Console.WriteLine("Чётные числа");
-         else Console.WriteLine("НеЧётные числа");
-     }
-}
-
-FillArray(array);  // метод заполняет массив
-PrintArray(array); // метод печатает массив
-*/
+Console.WriteLine("Количество четных элементов массива " + chet);
+Console.WriteLine("Количество нечётных элементов массива " + nechet);
