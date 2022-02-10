5fb82ed8674ad0212fd401dc20fc59369c936aab
alexfed84 <alfed09@gmail.com>
2022-02-10 14:18:20 +0300

    added task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Задача № 15:Найти третью цифру числа или сообщить, что её нет.
+
+Console.WriteLine("Введите  число:");
+string a = Console.ReadLine();
+int b = a.Length;
+
+if (b >= 3) Console.WriteLine( "Третья цифра числа: " + a[2]);
+else Console.WriteLine("Третья цифра в числе отсутсвует.");
