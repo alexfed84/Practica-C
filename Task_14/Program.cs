13b2df9952f2db7bb88de25ad657df8e0d0793b1
alexfed84 <alfed09@gmail.com>
2022-02-11 07:47:24 +0300

    task_14 done

---
+++ Task_14/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 4;
+Console.WriteLine("Введиет число от 1 до 10:");
+string b = Console.ReadLine();
+int c = Convert.ToInt32(b);
+if (a == c) Console.WriteLine("Введёное Вами число соотвествует заданному.");
+else Console.WriteLine("Введёное Вами число несоотвествует заданному.");
