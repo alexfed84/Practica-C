567bf8db419ca072e5bff9112b006a91f1d8ce9f
alexfed84 <alfed09@gmail.com>
2022-02-12 15:31:36 +0300

    task_39  start

---
+++ Task_39/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача № 39:	Найти произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int a = 0;
+string b = 0;
+int[] array = new int[a];
+
+
+void Input(int[] array)
+{  Console.WriteLine("Введите размер массива:");
+   b = Console.ReadLine();
+   a = Convert.ToInt32(b);
+}  
+
+void Solve(int[] array) 
+{    
+  for(int i=0; i<array.Length; i++)         
+     arr[i] =new Random().Next(0, 6); 
+}     
+         
+void Print(int[] arr)
+{      
+  for(int i=0;i<arr.Length;i++)        
+     System.Console.Write($"{arr[i]} ");  
+}  
+
+Input(a);
+Print(a); 
+Solve(a);
+System.Console.WriteLine();
+Print(a);
+
+
