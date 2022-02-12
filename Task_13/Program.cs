d83a00ea456c08fcf9c0dc08388c85cb6585da55
alexfed84 <alfed09@gmail.com>
2022-02-12 14:34:19 +0300

    task_41  done

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Задача № 13:	Удалить вторую цифру трёхзначного числа.
-
+/*
 string a = "123";
+int b = Convert.ToInt32(a);
+if(a[1] == 2) a[1] ='\0';
+Console.WriteLine(a);
 
 string Delete (string a, char OldValue, char NewValue)
 {
@@ -12,9 +15,10 @@ string Delete (string a, char OldValue, char NewValue)
     return result;
 }
 string newa = Delete(a, "2", "0");
+*/
+//Console.WriteLine("first line\nSecond line");
+Console.WriteLine(@"first line\nSecond line");
 
+//Console.WriteLine(newa);
 
-
-Console.WriteLine(newa);
-
-//Console.WriteLine("Get element by index s12[3]: " + s12[3]);
+//Console.WriteLine("Get element by index s12[3]: " + s12[3]); */
---
+++ Task_41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача № 41:	Выяснить являются ли три числа сторонами треугольника.
+
+int x = 0, y = 0, z = 0;
+
+
+void Input (out int x, out int y, out int z)
+{
+    string s = string.Empty;
+
+    System.Console.Write("Введите число 1 ");
+    s = Console.ReadLine();
+    x = Convert.ToInt32(s);
+
+    System.Console.Write("Введите число 2 ");
+    s = Console.ReadLine();
+    y = Convert.ToInt32(s);
+
+    System.Console.Write("Введите число 3 ");
+    s = Console.ReadLine();
+    z = Convert.ToInt32(s);
+}
+
+bool Solve(int a, int b, int c)
+{
+    bool f = false;
+    if(a < (b + c) && a > Math.Abs(b-c) && b < (a + c) && b > Math.Abs(a-c) && c < (b + a) && c > Math.Abs(b-a))
+    {
+        f= true;
+    }
+    return f;
+}
+
+void Print(bool r)
+{
+    if(r == true) System.Console.WriteLine("Введенные числа являются сторонами трегуольника");
+    if(r == false) System.Console.WriteLine("Введенные числа НЕ являются сторонами трегуольника");
+}
+
+Input(out x, out y, out z);
+Print(Solve(x,y,z));
+
+
+
+
+
+       
+
