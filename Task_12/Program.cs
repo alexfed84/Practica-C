1bffe94081cf36ebdcb3e957bc527961578f4455
alexfed84 <alfed09@gmail.com>
2022-02-13 19:35:46 +0300

    Task_12 done

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -1,4 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-int a = new Random().Next (10,99);
+﻿// Задача № 12:	Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+
+
+int a = new Random().Next(10,99);
 Console.WriteLine(a);
 
+int [] array = new int[2];
+int i = 0;
+while (i < array.Length)
+{
+    
+    array[i] = a%10;
+    a = a/10;
+    i++;
+}
+int max = array [0];
+
+while (i < array.Length)
+{
+    
+    if (array[i] > max) max = array[i];
+    i++;
+}
+
+Console.WriteLine("Наибольшая цифра числа: " + max);
