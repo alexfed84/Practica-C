d88c39a7b9c7283ae9a9c1994d2931e80178ab98
alexfed84 <alfed09@gmail.com>
2022-02-13 11:04:19 +0300

    task_06 done

---
+++ Task_06/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Задача № 6: Написать программу вычисления значения функции y = f(a).
+
+double F(double a)
+{
+    return a*2;
+}
+System.Console.WriteLine(F(6));
--- Task_39/Program.cs
+++ Task_39/Program.cs
@@ -1,32 +1,37 @@
 ﻿//Задача № 39:	Найти произведение пар чисел в одномерном массиве. 
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 int a = 0;
-string b = 0;
+string b = string.Empty;
 int[] array = new int[a];
 
 
-void Input(int[] array)
+void Input(int[] arr)
 {  Console.WriteLine("Введите размер массива:");
    b = Console.ReadLine();
    a = Convert.ToInt32(b);
 }  
 
-void Solve(int[] array) 
+void Solve(int[] arr) 
 {    
-  for(int i=0; i<array.Length; i++)         
+  for(int i=0; i<arr.Length; i++)         
      arr[i] =new Random().Next(0, 6); 
-}     
+  
+  for (int i = 0; i < arr.Length; i++)
+
+    System.Console.Write(arr[i] + " ");
+}
          
 void Print(int[] arr)
-{      
-  for(int i=0;i<arr.Length;i++)        
-     System.Console.Write($"{arr[i]} ");  
-}  
+{
+for (int i = 0; i < array.Length/2; i++)
 
-Input(a);
-Print(a); 
-Solve(a);
+    System.Console.Write(array[i]*array[a-1-i] + " ");
+}
+
+Input(arr);
+Print(arr); 
+Solve(arr);
 System.Console.WriteLine();
-Print(a);
+Print(array);
 
 
