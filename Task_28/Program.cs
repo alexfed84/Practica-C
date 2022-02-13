a07052a28544e5ee8dda15dfa8478bc186b225c2
alexfed84 <alfed09@gmail.com>
2022-02-13 13:46:25 +0300

    task_28 done

---
+++ Task_28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача № 28:	Определить сумму цифр в числе.
+
+int a = 0; 
+int sum = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите число ");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+while (a!= 0)
+{
+    sum = sum + a%10;
+    a = a/10;
+}
+
+System.Console.WriteLine("Сумма цифр в числе равна " + sum);
