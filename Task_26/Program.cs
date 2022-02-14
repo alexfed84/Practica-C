da34dbdae9b6f16670cf5e96b43085a7472614e4
alexfed84 <alfed09@gmail.com>
2022-02-14 08:36:13 +0300

    Task_26 done

---
+++ Task_26/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача № 26:	Возведите число А в натуральную степень B используя цикл.
+int a = 4;
+int b = 4;
+int res = 1;
+int i = 0;
+while (i<b)
+{
+    res = res * a;
+    i++;
+}
+
+Console.WriteLine(res);
+
