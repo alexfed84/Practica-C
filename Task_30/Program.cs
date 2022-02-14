053029f159aa49f4ecca43e3f7169c9b7cfe978e
alexfed84 <alfed09@gmail.com>
2022-02-14 13:13:52 +0300

    Task_43 done

--- Task_30/Program.cs
+++ Task_30/Program.cs
@@ -2,4 +2,4 @@
 int [] a = {1,2,3,4,5,6,7};
 for (int i=0; i<a.Length; i++)
 if (a[i]%2==0)
-Console.WriteLine("{0} {1}",a[i], Math.Pow(a[i],3) );
+Console.WriteLine("{0} в кубе {1}",a[i], Math.Pow(a[i],3) );
