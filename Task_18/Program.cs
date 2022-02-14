b9e4628f4b5f8df4523042b6762f562fdb14a7a1
alexfed84 <alfed09@gmail.com>
2022-02-14 08:30:53 +0300

    Task_18 done

---
+++ Task_18/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача № 18:	Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y.
+
+bool [] arrayX = {false, true};
+bool [] arrayY = {false, true};
+bool flag = true ;
+for(int i = 0; i<2; i++)
+{
+    for(int j = 0; j<2; j++)
+    {
+    
+      bool f1 = !(arrayX[i] || arrayY[j]);
+      bool f2 = (!arrayX[i] && !arrayY[j]);
+      if (f1!=f2)  
+      {
+       flag = false;
+       break;
+      }
+    
+    Console.WriteLine("{0,1} {1,8} {2,8}", arrayX[i], arrayY[j], f1, f2);
+    Console.WriteLine();
+    }
+}
+
+if (flag == true) Console.WriteLine("Выражение верно для всех значений X и Y");
+else Console.WriteLine("Выражение неверно для всех значений X и Y");
+
+
