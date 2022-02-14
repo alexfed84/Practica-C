3d68c770604633f3db4da3677b02cd45fa190202
alexfed84 <alfed09@gmail.com>
2022-02-14 16:48:10 +0300

    Task_29 done

---
+++ Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача № 29:	Написать программу вычисления произведения чисел от 1 до N.
+int [] a =  {1,2,3,4};
+int res = 1;
+for ( int i = 0; i<a.Length; i++)
+{
+    res = res * a[i];
+
+}
+Console.WriteLine ( res );
+
