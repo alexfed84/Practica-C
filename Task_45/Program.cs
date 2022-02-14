86b9d92f47d221046ac54b7fb7404794709090a7
alexfed84 <alfed09@gmail.com>
2022-02-14 13:37:00 +0300

    Task_45 done

---
+++ Task_45/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача № 45.	Показать числа Фибоначчи.
+int [] array =  new int [5];
+array[0] = 1;
+array[1] = 1;
+int sumarray;
+for (int i=0; i<array.Length; i++)
+{
+   array[i] = array[i-1] + array[i-2]; 
+   Console.WriteLine("ЧИсло Фибоначчи {0} равно {1}",i,array[i]);
+}
+
