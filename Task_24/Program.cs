85f558e0f23515588bd16c7f62be022d4256cfe2
alexfed84 <alfed09@gmail.com>
2022-02-16 11:16:39 +0300

    Task_25 done

--- Task_24/Program.cs
+++ Task_24/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 24:	Найти кубы чисел от 1 до N.
+
+int a=1; 
+int b=0;
+string s;
+
+Console.WriteLine("Введите число :");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+
+while(a <= b)
+{
+    Console.WriteLine ($"Значение числа {b} в кубе = {b*b*b}");
+    b++;
+    return;
+}
--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,5 +1,15 @@
 ﻿//  Задача № 25:Найти сумму чисел от 1 до А.
+int a=1; 
+int b=0;
+string s;
+int res =0;
+Console.WriteLine("Введите число :");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+for (int i = a; i<=b; i++)
+{
 
+  res = res + i;
 
-
-Console.WriteLine("Hello, World!");
+}
+Console.WriteLine ("Сумма чисел от 1 до " + b + " равна "+ res);
