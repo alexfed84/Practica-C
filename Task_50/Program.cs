09a6a666e8756c2265e6b3632ba8a89aba88730b
alexfed84 <alfed09@gmail.com>
2022-02-16 17:13:44 +0300

    Task_50 done

---
+++ Task_50/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача № 50:	В двумерном массиве n×k заменить четные элементы на противоположные.
+
+int [,] array = new int [2,4];
+int i=0;
+int j=0;
+//array[i,j] = new Random().Next();
+for( i = 0; i<array.GetLength(0); i++)
+{
+   for (j = 0; j<array.GetLength(1); j++)
+   {
+       array[i,j] = new Random().Next(1,8);
+       Console.Write($"{array[i,j]} " + "");
+       if (j%2 ==0) array[i,j]=array[i,j]*-1;
+       Console.Write ($"{array[i,j]} " + " ");
+   }
+  
+ Console.WriteLine ();
+ 
+}
+
+//Console.WriteLine ($"{array[i,j]} ");
