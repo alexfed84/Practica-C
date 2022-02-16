33a4594309adefd605955db528e4740b73865072
alexfed84 <alfed09@gmail.com>
2022-02-16 16:54:23 +0300

    Task_49 done

---
+++ Task_49/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача № 49:	Показать двумерный массив размером m×n заполненный вещественными числами.
+
+
+double [,] array = new double [3,4];
+
+for( int i = 0; i<array.GetLength(0); i++)
+{
+   for (int j = 0; j<array.GetLength(1); j++)
+   {
+       array[i,j] = new Random().NextDouble();
+       Console.Write ($"{array[i,j]} ");
+   }
+ Console.WriteLine ();
+
+}
+
