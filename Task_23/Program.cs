e304350f24126696b5e26e7d22aff1d69c0d9d4d
alexfed84 <alfed09@gmail.com>
2022-02-16 15:17:25 +0300

    Task_23 done

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23:	Показать таблицу квадратов чисел от 1 до N.
+
+int a=0;
+string s;
+int res =0;
+int i=1;
+Console.Write("Введите число: ");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+
+while( i <= a)
+{
+    Console.Write(i+" = ");
+    res = i * i;
+    System.Console.WriteLine(res+"; ");
+    i++;
+}
