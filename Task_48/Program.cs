b8c359fde284436a396872c3df26c5ff14966558
alexfed84 <alfed09@gmail.com>
2022-02-16 16:42:36 +0300

    Task_48 done

--- Task_48/Program.cs
+++ Task_48/Program.cs
@@ -1,5 +1,18 @@
-﻿// Задача № 48:	Показать двумерный массив размером m×n заполненный целыми числами.
+﻿// Задача № 48:	Показать двумерный массив размером m × n заполненный целыми числами.
 
+int [,] array = new int [3,4];
+int i=0;
+int j=0;
+//array[i,j] = new Random().Next();
+for( i = 0; i<array.GetLength(0); i++)
+{
+   for (j = 0; j<array.GetLength(1); j++)
+   {
+       array[i,j] = new Random().Next(1,8);
+       Console.Write ($"{array[i,j]} ");
+       
+   }
+ Console.WriteLine ();
 
+}
 
-Console.WriteLine("Hello, World!");
