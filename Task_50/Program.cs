666192a4cd31044f45c720842b64cc6d9134b6d7
alexfed84 <alfed09@gmail.com>
2022-02-16 17:14:22 +0300

    Task_50 done_1

--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -9,7 +9,7 @@ for( i = 0; i<array.GetLength(0); i++)
    for (j = 0; j<array.GetLength(1); j++)
    {
        array[i,j] = new Random().Next(1,8);
-       Console.Write($"{array[i,j]} " + "");
+       Console.Write($"{array[i,j]} " + " ");
        if (j%2 ==0) array[i,j]=array[i,j]*-1;
        Console.Write ($"{array[i,j]} " + " ");
    }
