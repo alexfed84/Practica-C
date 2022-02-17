cee7ad889171b6380d794c03477b21f5c586d501
alexfed84 <alfed09@gmail.com>
2022-02-17 09:46:24 +0300

    Task_40 start

---
+++ Task_40/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача № 40:	В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+int [] array = {2, 21, 57, 4, 9, 10, 33, 8, 13};
+
+int max = Max (
+ Max (array[0], array[1], array[2]),
+ Max (array[3], array[4], array[5]),
+ Max (array[6], array[7], array[8])
+);
+Console.WriteLine(max);
